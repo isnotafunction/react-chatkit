f99fda7665664889d0cf3c1eac7fb29805853a99
isnotafunction <ivana.cosgrove@gmail.com>
2018-07-03 23:13:52 +0100

    connects pusher's chatkit

--- App.js
+++ App.js
@@ -1,27 +1,49 @@
 import React from 'react'
+import Chatkit from '@pusher/chatkit'
 import MessageList from './MessageList/MessageList'
-import SendMessage from './SendMessage/SendMessage'
-import Rooms from './Rooms/Rooms'
-import AddRoom from './AddRoom/AddRoom'
+// import SendMessage from './SendMessage/SendMessage'
+// import Rooms from './Rooms/Rooms'
+// import AddRoom from './AddRoom/AddRoom'
 
+import {instanceLocator, tokenUrl} from './config'
 
 export default class App extends React.Component{
-  state = {
-   data: ''
+  constructor(){
+    super()
+    this.state = {
+      data: ''
+     }
   }
+  
+  componentDidMount(){
+   const chatManager = new Chatkit.ChatManager({
+     instanceLocator,
+     userId: 'ivi',
+     tokenProvider : new Chatkit.TokenProvider({
+       url: tokenUrl
+     })
+    })
 
-  async componentDidMount(){
-   const data = await fetch()
-   this.setState({data:data})
+     chatManager.connect()
+     .then(currentUser => {
+       currentUser.subscribeToRoom({
+         roomId: 10857405,
+         hooks: {
+            onNewMessage: message => {
+              console.log('message.text ', message.text)
+            }
+         }
+       })
+     })
   }
 
   render(){
     return (
       <React.Fragment>
-        <Rooms/>
-        <AddRoom/>
+        {/* <Rooms/>
+        <AddRoom/> */}
         <MessageList/>
-        <SendMessage/>
+        {/* <SendMessage/> */}
       </React.Fragment>
     )
 
