df0bc73bb3c4dbbe11fd9b9947d3b16ecd66ba56
isnotafunction <ivana.cosgrove@gmail.com>
2018-07-04 14:59:26 +0100

    styles Message component

--- components/Message/Message.js
+++ components/Message/Message.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const StyledContainer = styled.div`
+display:flex;
+flex-direction:column;
+justify-content:flex-start;
+margin:5px;
+padding: 5px;
+border-radius: 10px;
+background-color: #5d6b82;
+`
+const StyledUsername = styled.div`
+color: #ccf96b;
+opacity: 0.8;
+font-size: 12px;
+`
 const Message = ({username, text}) => {
     return (
-      <div>
-           <div> {username} </div>
+      <StyledContainer>
+           <StyledUsername> {username} </StyledUsername>
            <div> {text} </div>
-      </div>
+      </StyledContainer>
     )
   }
 
