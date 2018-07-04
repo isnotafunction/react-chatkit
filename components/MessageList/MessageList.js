69da4362225be356c4e8be5ac38cead46b5917d7
isnotafunction <ivana.cosgrove@gmail.com>
2018-07-04 15:32:18 +0100

    styles MessageList component

--- components/MessageList/MessageList.js
+++ components/MessageList/MessageList.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import Message from './../Message/Message'
+
+const StyledMessageList = styled.div`
+border: 1px solid #313e54;
+border-radius: 5px;
+grid-column: 2 / span 2;
+grid-row: 2 / 4;
+`
+
 
 export default class MessageList extends React.Component{
  render(){
    return (
-     <div>
+     <StyledMessageList>
        {this.props.messages.map((message, i) => {
          return(
            <Message key={`index${i}`} username={message.senderId} text={message.text}/>
          )
        })}
-     </div>
+     </StyledMessageList>
    )
  }
 }
