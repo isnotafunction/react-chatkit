f8d920bb6b18a76b34cd9209794914595969a6c6
isnotafunction <ivana.cosgrove@gmail.com>
2018-07-04 12:56:13 +0100

    removes dummy data

--- MessageList/MessageList.js
+++ MessageList/MessageList.js
@@ -1,20 +1,5 @@
 import React from 'react'
 
-const dummyData = [
-  {
-    sender: 'ivi',
-    text:'hi, how are you?'
-  },
-  {
-    sender: 'iva',
-    text:'Great, how are you, ivi!'
-  },
-  {
-    sender: 'ivi',
-    text:'awesome, thanks!'
-  }
-]
-
 export default class MessageList extends React.Component{
  render(){
    return (
