519d47738ab4d020bd2fb701000f5b7960df3f50
isnotafunction <ivana.cosgrove@gmail.com>
2018-07-05 11:59:24 +0100

    adds automatic scrolling

--- components/MessageList/MessageList.js
+++ components/MessageList/MessageList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import ReactDOM from 'react-dom'
 import Message from './../Message/Message'
 
 const StyledMessageList = styled.div`
@@ -8,10 +8,24 @@ border: 1px solid #313e54;
 border-radius: 5px;
 grid-column: 2 / span 2;
 grid-row: 2 / 4;
+overflow: scroll;
+max-height: 100%;
 `
 
-
 export default class MessageList extends React.Component{
+  
+  getSnapshotBeforeUpdate(){
+    const node = ReactDOM.findDOMNode(this)
+    this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+  }
+
+  componentDidUpdate(){
+    if(this.shouldScrollToBottom){
+      const node = ReactDOM.findDOMNode(this)
+      node.scrollTop = node.scrollHeight
+    }
+  }
+
  render(){
    return (
      <StyledMessageList>
